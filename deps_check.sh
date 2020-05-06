70430f7e8c51c9206178c2b38098ff0f1689573b
BiteDasher <cazorax@mail.ru>
2020-05-06 23:59:35 +0300

    Update repo

---
+++ deps_check.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+if ! [ "$(command -v valac)" ]; then echo "valac not found" >> .tmp; berror=1; fi
+if [ "$(pkg-config --exists gtk+-3.0; echo $?)" == 1 ]; then echo "GTK 3 not found" >> .tmp; berror=1; fi
+if [ "$(pkg-config --exists indicator3-0.4; echo $?)" == 1 ]; then echo "libindicator not found" >> .tmp; berror=1; fi
+if [ "$berror" ]; then cat .tmp; rm .tmp; else
+echo "All cool"; fi
