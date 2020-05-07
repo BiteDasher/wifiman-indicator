5ab9088d54d3ef59b3e4d5bb2f4edd3bc116ccab
BiteDasher <cazorax@mail.ru>
2020-05-07 12:03:43 +0300

    Update deps_check.sh

--- deps_check.sh
+++ deps_check.sh
@@ -4,5 +4,6 @@ if [ "$(pkg-config --exists gtk+-3.0; echo $?)" == 1 ]; then echo "GTK 3 not fou
 if [ "$(pkg-config --exists indicator3-0.4; echo $?)" == 1 ]; then echo "libindicator not found" >> .tmp; berror=1; fi
 if ! [ "$(command -v wifiman)" ]; then echo "wifiman not found" >> .tmp; berror=1; fi
 if ! [ -d /etc/systemd ]; then echo "systemd not found" >> .tmp; berror=1; fi
+if ! [ "$(command -v iwconfig)" ]; then echo "iwconfig not found" >> .tmp; berror=1; fi
 if [ "$berror" ]; then cat .tmp; rm .tmp; else
 echo "All cool"; fi
