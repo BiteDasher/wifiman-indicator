4777d921c02fae77a4235146aed2a0bac8a5bda3
BiteDasher <cazorax@mail.ru>
2020-05-07 18:09:21 +0300

    Update repo

--- wifistart.sh
+++ wifistart.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/bash
-if [ $1 ]; then 
+if [ $1 ]; then
+case $1 in
+toggle)
 if [ "$(cat /etc/xdg/autostart/indicator.desktop | grep "#")" ]; then
 sed -i /etc/xdg/autostart/indicator.desktop -e 's/\#\[Desktop\ Entry\]/\[Desktop\ Entry\]/g'; echo Added to autoload; else
-sed -i /etc/xdg/autostart/indicator.desktop -e 's/\[Desktop\ Entry\]/\#\[Desktop\ Entry\]/g'; echo Removed from autoload; fi; exit 0; fi
+sed -i /etc/xdg/autostart/indicator.desktop -e 's/\[Desktop\ Entry\]/\#\[Desktop\ Entry\]/g'; echo Removed from autoload; fi; exit 0
+;;
+start)
+vindicator /tmp/signal "wifistart.sh" &>/dev/null 2>&1 & disown
+;;
+stop)
+killall vindicator
+;;
+esac
+fi
 if ! [ "$(command -v wifigui)" ]; then zenity --error --title="Error" --text="WifiMan not found!"; exit 1; fi
 zenity --password --title="sudo password" | sudo -S "wifigui"
