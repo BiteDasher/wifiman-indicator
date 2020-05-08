30b30c90631bfee1e66cccee5f36686e963ff85c
BiteDasher <cazorax@mail.ru>
2020-05-08 19:15:47 +0300

    Change /usr/bin/bash to /bin/bash

--- debian_patch.sh
+++ debian_patch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 sed "s/PREFIX\ \:\=\ \/usr\/local/PREFIX\ \:\=\ \/usr/g" -i Makefile
 sed 's/PK_EXEC_DEST \:\= \\\/usr\\\/local\\\/bin/PK_EXEC_DEST \:\= \\\/usr\\\/bin/g' -i Makefile
 sed "s/iwconfig/\/usr\/sbin\/iwconfig/g" -i get_iw_signal
--- deps_check.sh
+++ deps_check.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 if ! [ "$(command -v valac)" ]; then echo "valac not found" >> .tmp; berror=1; fi
 if [ "$(pkg-config --exists gtk+-3.0; echo $?)" == 1 ]; then echo "GTK 3 not found" >> .tmp; berror=1; fi
 if [ "$(pkg-config --exists indicator3-0.4; echo $?)" == 1 ]; then 
